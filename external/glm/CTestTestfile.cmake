abecd54ee66efd98c28564dadd4a55ad3e32711d
Thomas Ray <the3dvehicleguy@gmail.com>
2024-10-29 23:04:28 -0400

    cleanup include and library dir

---
+++ external/glm/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: G:/ART_REPO-TRAY/X-Plane 12/Scenery-Editor-X/external/glm
+# Build directory: G:/ART_REPO-TRAY/X-Plane 12/Scenery-Editor-X/external/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
+subdirs("test")
