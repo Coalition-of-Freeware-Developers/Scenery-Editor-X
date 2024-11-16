34bfb4062b1b6497a024e41089d57998c486274b
John Parsaie <parsaiej@gmail.com>
2024-11-16 01:37:50 -0500

    Initial lobotomy.

--- external/glm/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/ART_REPO-TRAY/X-Plane 12/Scenery-Editor-X/external/glm
-# Build directory: G:/ART_REPO-TRAY/X-Plane 12/Scenery-Editor-X/external/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glm")
-subdirs("test")
