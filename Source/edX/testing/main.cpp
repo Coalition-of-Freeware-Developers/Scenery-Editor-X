080abcfb2cc048b0ebe2c3375031490dc55e7c0b
Thomas Ray <the3dvehicleguy@gmail.com>
2024-11-21 03:14:59 -0500

    add cmake entry and source files for FileTester application

--- Source/edX/testing/main.cpp
+++ Source/edX/testing/main.cpp
@@ -1,232 +1,241 @@
-// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
-// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <imgui.h>
+#include <GLFW/glfw3.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_vulkan.h>
+#include <vulkan/vulkan.h>
+#include "../edXProjectFile.h"
 
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include <stdio.h>
-#define GL_SILENCE_DEPRECATION
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-#include <GLES2/gl2.h>
-#endif
-#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+// Function prototypes
+void CheckVkResult(VkResult err);
+void SetupVulkan(VkInstance &instance,
+                 VkDevice &device,
+                 VkPhysicalDevice &physicalDevice,
+                 VkQueue &queue,
+                 VkCommandPool &commandPool,
+                 VkDescriptorPool &descriptorPool);
+void CleanupVulkan(VkInstance instance, VkDevice device, VkCommandPool commandPool, VkDescriptorPool descriptorPool);
 
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
-// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
-// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
-#pragma comment(lib, "legacy_stdio_definitions")
-#endif
+void SaveProject(const std::string &directory, const ProjectFile::projectFile &project);
+void LoadProject(const std::string &filePath, ProjectFile::projectFile &project);
 
-// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
-#ifdef __EMSCRIPTEN__
-#include "../libs/emscripten/emscripten_mainloop_stub.h"
-#endif
-
-static void glfw_error_callback(int error, const char *description)
+int main()
 {
-    fprintf(stderr, "GLFW Error %d: %s\n", error, description);
-}
-
-// Main code
-int main(int, char **)
-{
-    glfwSetErrorCallback(glfw_error_callback);
+    // Initialize GLFW
     if (!glfwInit())
-        return 1;
+    {
+        std::cerr << "Failed to initialize GLFW!" << std::endl;
+        return -1;
+    }
 
-        // Decide GL+GLSL versions
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-    // GL ES 2.0 + GLSL 100
-    const char *glsl_version = "#version 100";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-#elif defined(__APPLE__)
-    // GL 3.2 + GLSL 150
-    const char *glsl_version = "#version 150";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // Required on Mac
-#else
-    // GL 3.0 + GLSL 130
-    const char *glsl_version = "#version 130";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-#endif
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // Vulkan support
+    GLFWwindow *window = glfwCreateWindow(800, 600, "edX File Format Tester - Vulkan", nullptr, nullptr);
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
-    // Create window with graphics context
-    GLFWwindow *window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
-    if (window == nullptr)
-        return 1;
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
+    // Vulkan setup
+    VkInstance instance;
+    VkDevice device;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkQueue queue;
+    VkCommandPool commandPool;
+    VkDescriptorPool descriptorPool;
+    SetupVulkan(instance, device, physicalDevice, queue, commandPool, descriptorPool);
 
-    // Setup Dear ImGui context
+    // Initialize ImGui for Vulkan
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
     (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;  // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;     // Enable Docking
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
-    //io.ConfigViewportsNoAutoMerge = true;
-    //io.ConfigViewportsNoTaskBarIcon = true;
 
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-    //ImGui::StyleColorsLight();
+    ImGui_ImplGlfw_InitForVulkan(window, true);
 
-    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
-    ImGuiStyle &style = ImGui::GetStyle();
-    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-    {
-        style.WindowRounding = 0.0f;
-        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-    }
+    ImGui_ImplVulkan_InitInfo init_info = {};
+    init_info.Instance = instance;
+    init_info.PhysicalDevice = physicalDevice;
+    init_info.Device = device;
+    init_info.QueueFamily = 0; // Set appropriate queue family index
+    init_info.Queue = queue;
+    init_info.PipelineCache = VK_NULL_HANDLE;
+    init_info.DescriptorPool = descriptorPool;
+    init_info.Subpass = 0;
+    init_info.MinImageCount = 2;
+    init_info.ImageCount = 2;
+    init_info.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
+    init_info.Allocator = nullptr;
+    init_info.CheckVkResultFn = CheckVkResult;
+    ImGui_ImplVulkan_Init(&init_info);
 
-    // Setup Platform/Renderer backends
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-#ifdef __EMSCRIPTEN__
-    ImGui_ImplGlfw_InstallEmscriptenCallbacks(window, "#canvas");
-#endif
-    ImGui_ImplOpenGL3_Init(glsl_version);
-
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != nullptr);
-
-    // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    // Upload fonts (Vulkan requires this step)
+    VkCommandBuffer commandBuffer = VK_NULL_HANDLE; // Create and allocate a command buffer
+    ImGui_ImplVulkan_CreateFontsTexture();
 
     // Main loop
-#ifdef __EMSCRIPTEN__
-    // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
-    // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
-    io.IniFilename = nullptr;
-    EMSCRIPTEN_MAINLOOP_BEGIN
-#else
-    while (!glfwWindowShouldClose(window))
-#endif
-    {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        glfwPollEvents();
-        if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0)
-        {
-            ImGui_ImplGlfw_Sleep(10);
-            continue;
-        }
+    ProjectFile::projectFile project = {"example", "New Scenery", "1.0", "12.00"};
+    std::string directory = std::filesystem::current_path().string();
+    char fileNameBuffer[256] = "example.edX";
 
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // Start ImGui frame
+        ImGui_ImplVulkan_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        // Main window
+        ImGui::Begin("edX File Format Tester");
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        ImGui::InputText("Scenery Name", project.sceneryName.data(), 256);
+        ImGui::InputText("Editor Version", project.editorVersion.data(), 256);
+        ImGui::InputText("XP Version", project.XPVersion.data(), 256);
+        ImGui::InputText("File Name", fileNameBuffer, 256);
+
+        ImGui::Text("Current Directory: %s", directory.c_str());
+        if (ImGui::Button("Select Directory"))
         {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");          // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);             // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button(
-                    "Button")) // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
+            std::cout << "Directory selection not implemented in this example." << std::endl;
         }
 
-        // 3. Show another simple window.
-        if (show_another_window)
+        if (ImGui::Button("Save File"))
         {
-            ImGui::Begin(
-                "Another Window",
-                &show_another_window); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
+            project.filename = fileNameBuffer;
+            SaveProject(directory, project);
         }
+
+        if (ImGui::Button("Load File"))
+        {
+            LoadProject(directory + "/" + fileNameBuffer, project);
+        }
+
+        ImGui::End();
 
         // Rendering
         ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(clear_color.x * clear_color.w,
-                     clear_color.y * clear_color.w,
-                     clear_color.z * clear_color.w,
-                     clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-        // Update and Render additional Platform Windows
-        // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
-        //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
-        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-        {
-            GLFWwindow *backup_current_context = glfwGetCurrentContext();
-            ImGui::UpdatePlatformWindows();
-            ImGui::RenderPlatformWindowsDefault();
-            glfwMakeContextCurrent(backup_current_context);
-        }
-
-        glfwSwapBuffers(window);
+        // Add Vulkan rendering commands here
     }
-#ifdef __EMSCRIPTEN__
-    EMSCRIPTEN_MAINLOOP_END;
-#endif
 
     // Cleanup
-    ImGui_ImplOpenGL3_Shutdown();
+    vkDeviceWaitIdle(device);
+    ImGui_ImplVulkan_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
 
+    CleanupVulkan(instance, device, commandPool, descriptorPool);
     glfwDestroyWindow(window);
     glfwTerminate();
 
     return 0;
+}
+
+// Vulkan setup functions (simplified)
+void CheckVkResult(VkResult err)
+{
+    if (err != VK_SUCCESS)
+    {
+        std::cerr << "Vulkan error: " << err << std::endl;
+        abort();
+    }
+}
+
+void SetupVulkan(VkInstance &instance,
+                 VkDevice &device,
+                 VkPhysicalDevice &physicalDevice,
+                 VkQueue &queue,
+                 VkCommandPool &commandPool,
+                 VkDescriptorPool &descriptorPool)
+{
+    // Instance creation
+    VkInstanceCreateInfo instanceCreateInfo = {};
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    CheckVkResult(vkCreateInstance(&instanceCreateInfo, nullptr, &instance));
+
+    // Device creation and queue setup
+    // (Simplified: Ensure to enumerate physical devices and choose the appropriate one)
+    physicalDevice = VK_NULL_HANDLE; // Assign proper device
+    VkDeviceCreateInfo deviceCreateInfo = {};
+    deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    CheckVkResult(vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device));
+
+    // Queue retrieval
+    vkGetDeviceQueue(device, 0, 0, &queue);
+
+    // Command pool
+    VkCommandPoolCreateInfo commandPoolCreateInfo = {};
+    commandPoolCreateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    CheckVkResult(vkCreateCommandPool(device, &commandPoolCreateInfo, nullptr, &commandPool));
+
+    // Descriptor pool
+    VkDescriptorPoolSize poolSizes[] = {{VK_DESCRIPTOR_TYPE_SAMPLER, 1000},
+                                        {VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1000},
+                                        {VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE, 1000},
+                                        {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1000},
+                                        {VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER, 1000},
+                                        {VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER, 1000},
+                                        {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, 1000},
+                                        {VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, 1000},
+                                        {VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC, 1000},
+                                        {VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC, 1000},
+                                        {VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 1000}};
+    VkDescriptorPoolCreateInfo descriptorPoolCreateInfo = {};
+    descriptorPoolCreateInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+    descriptorPoolCreateInfo.maxSets = 1000;
+    descriptorPoolCreateInfo.poolSizeCount = static_cast<uint32_t>(std::size(poolSizes));
+    descriptorPoolCreateInfo.pPoolSizes = poolSizes;
+    CheckVkResult(vkCreateDescriptorPool(device, &descriptorPoolCreateInfo, nullptr, &descriptorPool));
+}
+
+void CleanupVulkan(VkInstance instance, VkDevice device, VkCommandPool commandPool, VkDescriptorPool descriptorPool)
+{
+    vkDestroyDescriptorPool(device, descriptorPool, nullptr);
+    vkDestroyCommandPool(device, commandPool, nullptr);
+    vkDestroyDevice(device, nullptr);
+    vkDestroyInstance(instance, nullptr);
+}
+
+// File save/load implementations
+void SaveProject(const std::string &directory, const ProjectFile::projectFile &project)
+{
+    std::ofstream outFile(directory + "/" + project.filename + ".edX");
+    if (!outFile)
+    {
+        std::cerr << "Failed to open file for writing!" << std::endl;
+        return;
+    }
+    outFile << "SceneryName: " << project.sceneryName << "\n";
+    outFile << "EditorVersion: " << project.editorVersion << "\n";
+    outFile << "XPVersion: " << project.XPVersion << "\n";
+    outFile.close();
+    std::cout << "File saved to " << directory + "/" + project.filename + ".edX" << std::endl;
+}
+
+void LoadProject(const std::string &filePath, ProjectFile::projectFile &project)
+{
+    std::ifstream inFile(filePath);
+    if (!inFile)
+    {
+        std::cerr << "Failed to open file for reading!" << std::endl;
+        return;
+    }
+    std::string line;
+    while (std::getline(inFile, line))
+    {
+        if (line.find("SceneryName:") != std::string::npos)
+            project.sceneryName = line.substr(line.find(":") + 1);
+        else if (line.find("EditorVersion:") != std::string::npos)
+            project.editorVersion = line.substr(line.find(":") + 1);
+        else if (line.find("XPVersion:") != std::string::npos)
+            project.XPVersion = line.substr(line.find(":") + 1);
+    }
+    inFile.close();
+    std::cout << "File loaded from " << filePath << std::endl;
 }
