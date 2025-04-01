61d49107ba2427e93226650bc5e524287839bcdf
Thomas Ray <the3dvehicleguy@gmail.com>
2025-04-01 17:57:36 -0400

    adding back archive files for reference
    Signed-off-by: Thomas Ray <the3dvehicleguy@gmail.com>

---
+++ source/_archive/SceneryEditorX/core/Memory.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdlib>
+#include <limits>
+#include <map>
+#include <mutex>
+#include <utility>
+
+namespace SceneryEditorX
+{
+    struct 
+    {
+
+    };
+}
---
+++ source/_archive/SceneryEditorX/scene/Scene.cpp
@@ -0,0 +1,26 @@
+#include "../xpeditorpch.h"
+#include "../src/scene/Scene.h"
+
+namespace SceneryEditorX
+{
+    void Scene::OnCreate(SceneType, SceneViewType)
+    {
+
+    }
+    
+    void Scene::OnUpdate(Timestep ts)
+    {
+
+    }
+    
+    void Scene::OnEvent(Event &e)
+    {
+
+    }
+    
+    void Scene::OnImGuiRender()
+    {
+
+    }
+
+    } // namespace SceneryEditorX
