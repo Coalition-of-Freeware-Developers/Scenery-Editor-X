ab7f4f72375a64791c36dea9dbff0d4db7dc011a
Thomas Ray <the3dvehicleguy@gmail.com>
2025-04-01 17:50:09 -0400

    cleanup Scripts directory
    Signed-off-by: Thomas Ray <the3dvehicleguy@gmail.com>

--- Scripts/vk_shader_compile.bat
+++
@@ -1,9 +0,0 @@
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe ..\shaders\light.vert -o   ..\shaders\light.vert.spv
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe ..\shaders\light.frag -o   ..\shaders\light.frag.spv
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe ..\shaders\opaque.frag -o  ..\shaders\opaque.frag.spv
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe ..\shaders\opaque.vert -o  ..\shaders\opaque.vert.spv
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe ..\shaders\pbr.vert -o     ..\shaders\pbr.vert.spv
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe ..\shaders\pbr.frag -o     ..\shaders\pbr.frag.spv
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe ..\shaders\present.vert -o ..\shaders\present.vert.spv
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe ..\shaders\present.frag -o ..\shaders\present.frag.spv
-pause
