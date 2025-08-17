7da159f98be6c188c05f5cb341cab2f710b09a54
Thomas Ray <the3dvehicleguy@gmail.com>
2025-08-17 19:03:34 -0400

    replace ImGuizmo with vcpkg port

--- source/Math/includes/dot.h
+++ source/Math/includes/dot.h
@@ -12,18 +12,16 @@
 */
 #pragma once
 #include <Math/math_config.h>
-#include <Math/includes/vec2.h>
-#include <Math/includes/vec3.h>
-#include <Math/includes/vec4.h>
+#include <Math/includes/vector.h>
 
 /// -----------------------------------------------------
 
 namespace SceneryEditorX
 {
-    // Local aliases (duplicated from vector.h for standalone inclusion safety)
-    using Vec2 = Utils::TVector2<float>;
-    using Vec3 = Utils::TVector3<float>;
-    using Vec4 = Utils::TVector4<float>;
+	// Local aliases (duplicated from vector.h for standalone inclusion safety)
+	using Vec2 = Utils::TVector2<float>;
+	using Vec3 = Utils::TVector3<float>;
+	using Vec4 = Utils::TVector4<float>;
 
 	inline float Dot(const Vec2& a, const Vec2& b) noexcept { return a.x * b.x + a.y * b.y; }
 	inline float Dot(const Vec3& a, const Vec3& b) noexcept { return a.x * b.x + a.y * b.y + a.z * b.z; }
@@ -32,8 +30,8 @@ namespace SceneryEditorX
 	inline Vec3 Cross(const Vec3& a, const Vec3& b) noexcept
 	{
 		return Vec3{ a.y * b.z - a.z * b.y,
-			        a.z * b.x - a.x * b.z,
-			        a.x * b.y - a.y * b.x };
+		        a.z * b.x - a.x * b.z,
+		        a.x * b.y - a.y * b.x };
 	}
 }
 
