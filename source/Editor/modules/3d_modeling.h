99c87a9f3bb1541f0d16511d14c5a17946185b97
Thomas Ray <the3dvehicleguy@gmail.com>
2025-08-17 20:25:30 -0400

    init modeling functionality

--- source/Editor/modules/3d_modeling.h
+++ source/Editor/modules/3d_modeling.h
@@ -5,19 +5,24 @@
 * Copyright (c) 2025 Thomas Ray
 * Copyright (c) 2025 Coalition of Freeware Developers
 * -------------------------------------------------------
-* spline_module.h
+* 3d_modeling.h
 * -------------------------------------------------------
-* Created: 13/8/2025
+* Created: 17/8/2025
 * -------------------------------------------------------
 */
 #pragma once
+#include "SceneryEditorX/renderer/buffers/vertex_buffer.h"
 #include "SceneryEditorX/core/modules/module.h"
-
-/// -------------------------------------------------------
+/// ----------------------------------------------------------
 
 namespace SceneryEditorX
 {
+	class Modeling : public Module
+	{
+	public:
 
+	}
 }
 
-/// -------------------------------------------------------
+/// ----------------------------------------------------------
+
