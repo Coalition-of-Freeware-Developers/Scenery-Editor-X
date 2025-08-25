06b90c0dfca7b4989005c33e9aec607998c0d097
Thomas Ray <the3dvehicleguy@gmail.com>
2025-08-25 01:59:07 -0400

    Corrected Viewport Data

--- source/SceneryEditorX/renderer/viewport.cpp
+++ source/SceneryEditorX/renderer/viewport.cpp
@@ -16,16 +16,94 @@
 
 namespace SceneryEditorX
 {
+    // Constructors
+    Viewport::Viewport(const float x, const float y, const float width, const float height, const float minDepth, const float maxDepth) :
+        m_Data{
+            .x = x, .y = y,
+            .width = width, .height = height,
+            .minDepth = minDepth, .maxDepth = maxDepth}
+    {
+    }
 
-	bool Viewport::operator==(const Viewport &data) const
+    Viewport::Viewport(const Viewport &viewport) : Resource(viewport), m_Data(viewport.m_Data)
+    {
+        viewportHovered = viewport.viewportHovered;
+        viewportResized = viewport.viewportResized;
+    }
+    
+    Viewport::Viewport(const ViewportData& data) : m_Data(data)
+    {
+    }
+
+    // Operators
+    bool Viewport::operator==(const Viewport &data) const
+    {
+        return m_Data.x == data.m_Data.x && 
+               m_Data.y == data.m_Data.y && 
+               m_Data.width == data.m_Data.width && 
+               m_Data.height == data.m_Data.height && 
+               m_Data.minDepth == data.m_Data.minDepth && 
+               m_Data.maxDepth == data.m_Data.maxDepth;
+    }
+
+    // Methods
+    float Viewport::GetAspectRatio() const
+    {
+        return (m_Data.height != 0.0f) ? (m_Data.width / m_Data.height) : 0.0f;
+    }
+
+    bool Viewport::IsDefined() const
+    {
+        return m_Data.x != 0.0f || m_Data.y != 0.0f || 
+               m_Data.width != 0.0f || m_Data.height != 0.0f || 
+               m_Data.minDepth != 0.0f || m_Data.maxDepth != 0.0f;
+    }
+
+	void Viewport::SetData(const ViewportData& data)
 	{
-	    return x == data.x && y == data.y && width == data.width && height == data.height && minDepth == data.minDepth && maxDepth == data.maxDepth;
+	    // Use placement new to reconstruct the ViewportData with new values
+	    m_Data.~ViewportData();
+	    hnew (&m_Data) ViewportData(data);
+	    viewportResized = true;
 	}
-	
-	bool Viewport::IsDefined() const
-	{
-	    return x != 0.0f || y != 0.0f || width != 0.0f || height != 0.0f || minDepth != 0.0f || maxDepth != 0.0f;
-	}
+
+    // Setters for mutable operations
+    void Viewport::SetPosition(float x, float y)
+    {
+        m_Data.~ViewportData();
+        hnew (&m_Data) ViewportData{
+            .x = x, .y = y,
+            .width = m_Data.width, .height = m_Data.height,
+            .minDepth = m_Data.minDepth, .maxDepth = m_Data.maxDepth
+        };
+        viewportResized = true;
+    }
+    
+    void Viewport::SetSize(float width, float height)
+    {
+        m_Data.~ViewportData();
+        hnew (&m_Data) ViewportData{
+            .x = m_Data.x, .y = m_Data.y,
+            .width = width, .height = height,
+            .minDepth = m_Data.minDepth, .maxDepth = m_Data.maxDepth
+        };
+        viewportResized = true;
+    }
+    
+    void Viewport::SetDepthRange(float minDepth, float maxDepth)
+    {
+        m_Data.~ViewportData();
+        hnew (&m_Data) ViewportData{
+            .x = m_Data.x, .y = m_Data.y,
+            .width = m_Data.width, .height = m_Data.height,
+            .minDepth = minDepth, .maxDepth = maxDepth
+        };
+    }
+
+    const Viewport& Viewport::GetUndefined()
+    {
+        return s_Undefined;
+    }
 
     const Ref<Viewport> undefined = CreateRef<Viewport>(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
 
--- source/SceneryEditorX/renderer/viewport.h
+++ source/SceneryEditorX/renderer/viewport.h
@@ -12,6 +12,7 @@
 */
 #pragma once
 #include "vulkan/resource.h"
+#include "vulkan/vk_data.h"
 
 /// -------------------------------------------------------
 
@@ -21,32 +22,33 @@ namespace SceneryEditorX
     class Viewport : public Resource
     {
     public:
-        explicit Viewport::Viewport(const float x = 0.0f, const float y = 0.0f, const float width = 0.0f, const float height = 0.0f, const float minDepth = 0.0f, const float maxDepth = 1.0f)
-		{
-		    this->x			= x;
-		    this->y			= y;
-		    this->width		= width;
-		    this->height	= height;
-		    this->minDepth  = minDepth;
-		    this->maxDepth  = maxDepth;
-		}
-
-		Viewport::Viewport(const Viewport &viewport) : Resource(viewport)
-        {
-		    x		 = viewport.x;
-		    y		 = viewport.y;
-		    width	 = viewport.width;
-		    height	 = viewport.height;
-		    minDepth = viewport.minDepth;
-		    maxDepth = viewport.maxDepth;
-		}
-
+        explicit Viewport(const float x = 0.0f, const float y = 0.0f, const float width = 0.0f, const float height = 0.0f, const float minDepth = 0.0f, const float maxDepth = 1.0f);
+        Viewport(const Viewport &viewport);
+        Viewport(const ViewportData& data);
         virtual ~Viewport() = default;
 
         bool operator==(const Viewport &data) const;
         bool operator!=(const Viewport &data) const { return !(*this == data); }
-		float GetAspectRatio() const { return width / height; }
+
+        float GetAspectRatio() const;
         bool IsDefined() const;
+        
+        // Access to viewport data
+        const ViewportData& GetData() const { return m_Data; }
+        void SetData(const ViewportData &data);
+        
+        // Convenience accessors
+        float GetX() const { return m_Data.x; }
+        float GetY() const { return m_Data.y; }
+        float GetWidth() const { return m_Data.width; }
+        float GetHeight() const { return m_Data.height; }
+        float GetMinDepth() const { return m_Data.minDepth; }
+        float GetMaxDepth() const { return m_Data.maxDepth; }
+        
+        // Setters for mutable operations
+        void SetPosition(float x, float y);
+        void SetSize(float width, float height);
+        void SetDepthRange(float minDepth, float maxDepth);
 
         /** @brief Flag indicating whether the mouse is currently hovering over the viewport */
         bool viewportHovered = false;
@@ -54,14 +56,15 @@ namespace SceneryEditorX
         /** @brief Flag indicating whether the viewport has been resized and needs updating */
         bool viewportResized = false;
 
-        float x		 = 0.0f;
-        float y		 = 0.0f;
-        float width	 = 0.0f;
-        float height	 = 0.0f;
-        float minDepth = 0.0f;
-        float maxDepth = 1.0f;
+        // Static undefined viewport instance
+        GLOBAL const Viewport& GetUndefined();
 
-        GLOBAL const Viewport undefined;
+    private:
+        /** @brief Core viewport data using the existing ViewportData struct */
+        ViewportData m_Data;
+        
+        // Static undefined instance - declared here, defined in source
+        INTERNAL Viewport s_Undefined;
     };
 
 }
